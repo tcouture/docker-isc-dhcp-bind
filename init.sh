9cd7c418a3ec19972130f7cac338fa382d053df0
Thierry Couture <thierry@arabrab.com>
2020-04-28 21:05:16 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,11 +9,11 @@ set -e
 #
 # Define default Variables.
 #
-USER="named"
-GROUP="named"
+USER="isc"
+GROUP="isc"
 COMMAND_OPTIONS_DEFAULT="-f"
-NAMED_UID_DEFAULT="1000"
-NAMED_GID_DEFAULT="101"
+ISC_UID_DEFAULT="1000"
+ISC_GID_DEFAULT="101"
 COMMAND_BIND="/usr/sbin/named -u ${USER} -c /etc/bind/named.conf ${COMMAND_OPTIONS:=${COMMAND_OPTIONS_DEFAULT}}"
 COMMAND_DHCP="/usr/sbin/dhcpd -4 -f -d --no-pid -cf /etc/dhcp/dhcpd.conf"
 
@@ -28,10 +28,10 @@ echo "=============="
 echo
 echo "  Username:        ${USER}"
 echo "  Groupname:       ${GROUP}"
-echo "  UID actual:      ${NAMED_UID_ACTUAL}"
-echo "  GID actual:      ${NAMED_GID_ACTUAL}"
-echo "  UID prefered:    ${NAMED_UID:=${NAMED_UID_DEFAULT}}"
-echo "  GID prefered:    ${NAMED_GID:=${NAMED_GID_DEFAULT}}"
+echo "  UID actual:      ${ISC_UID_ACTUAL}"
+echo "  GID actual:      ${ISC_GID_ACTUAL}"
+echo "  UID prefered:    ${ISC_UID:=${ISC_UID_DEFAULT}}"
+echo "  GID prefered:    ${ISC_GID:=${ISC_GID_DEFAULT}}"
 echo "  Command:         ${COMMAND_BIND}"
 echo "  Command:         ${COMMAND_DHCP}"
 echo
@@ -40,35 +40,36 @@ echo
 # Change UID / GID of named user.
 #
 echo "Updating UID / GID... "
-if [[ ${NAMED_GID_ACTUAL} -ne ${NAMED_GID} -o ${NAMED_UID_ACTUAL} -ne ${NAMED_UID} ]]
+if [[ ${ISC_GID_ACTUAL} -ne ${ISC_GID} -o ${ISC_UID_ACTUAL} -ne ${ISC_UID} ]]
 then
     echo "change user / group"
     deluser ${USER}
-    addgroup -g ${NAMED_GID} ${GROUP}
-    adduser -u ${NAMED_UID} -G ${GROUP} -h /etc/bind -g 'Linux User named' -s /sbin/nologin -D ${USER}
+    addgroup -g ${ISC_GID} ${GROUP}
+    adduser -u ${ISC_UID} -G ${GROUP} -h /etc/bind -g 'Linux User named' -s /sbin/nologin -D ${USER}
     echo "[DONE]"
     echo "Set owner and permissions for old uid/gid files"
-    find / -not \( -path /proc -prune \) -not \( -path /sys -prune \) -user ${NAMED_UID_ACTUAL} -exec chown ${USER} {} \;
-    find / -not \( -path /proc -prune \) -not \( -path /sys -prune \) -group ${NAMED_GID_ACTUAL} -exec chgrp ${GROUP} {} \;
+    find / -not \( -path /proc -prune \) -not \( -path /sys -prune \) -user ${ISC_UID_ACTUAL} -exec chown ${USER} {} \;
+    find / -not \( -path /proc -prune \) -not \( -path /sys -prune \) -group ${ISC_GID_ACTUAL} -exec chgrp ${GROUP} {} \;
     echo "[DONE]"
 else
     echo "[NOTHING DONE]"
 fi
 
+touch /var/lib/dhcpd/dhcpd.leases
+
 #
 # Set owner and permissions.
 #
 echo "Set owner and permissions... "
-chown -R ${USER}:${GROUP} /var/bind /etc/bind /var/run/named /var/log/named
-chmod -R o-rwx /var/bind /etc/bind /var/run/named /var/log/named
+chown -R ${USER}:${GROUP} /var/bind /etc/bind /var/run/named /var/log/named /var/lib/dhcpd/dhcpd.leases
+chmod -R o-rwx /var/bind /etc/bind /var/run/named /var/log/named /var/lib/dhcpd/dhcpd.leases
 echo "[DONE]"
-
-touch /var/lib/dhcpd/dhcpd.leases
 
 #
 # Start named.
 #
 echo "Start named... "
 exec ${COMMAND_BIND}
+#
 echo "Start dhcpd... "
 exec ${COMMAND_DHCP}
